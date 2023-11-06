1bcb5b4b792c567ad41c085d79134f73b0849f33
ARDA GUNSUREN <ardagunsuren@gmail.com>
2023-11-06 10:48:44 +0200

    Fix bug

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ module.exports = {
     baseUrl: "https://api.elevenlabs.io/v1/",
     defaultVoiceSettings: {
         stability: 0.95,
-        similarityBoost: 0.75,
+        similarity_boost: 0.75,
         style: 0.06,
-        useSpeakerBoost: true
+        use_speaker_boost: true
     }
 }
